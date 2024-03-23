e4c64c5aa1f9a65378b8cb6f1d87f320cc3f5b9d
jameslee1237 <zcabjyl@ucl.ac.uk>
2024-03-23 17:08:43 +0900

    designing layout

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -4,11 +4,16 @@ import React from "react";
 
 export default function Home() {
     return (
-        <div className="flex">    
-            <div className="flex bg-blue-200 h-[15vh]">
-                <UserButton />
+        <div className="flex flex-col">    
+            <div className="flex bg-blue-200 h-[10vh] justify-between">
+                <div className="flex ml-4 items-center">
+                    <h1 className="font-bold text-[20px]">DreamCanvas</h1>
+                </div>
+                <div className="flex mr-4 items-center">
+                    <UserButton />
+                </div>
             </div>
-            <div className="flex bg-blue-400 h-[85vh]">
+            <div className="flex bg-blue-400 h-[90vh]">
                 Login Complete!
             </div>
         </div>
