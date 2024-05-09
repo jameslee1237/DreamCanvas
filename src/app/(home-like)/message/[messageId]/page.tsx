656c431d3b7ef1239a4dd1d12bd8ddd8ffe051f7
jameslee1237 <zcabjyl@ucl.ac.uk>
2024-05-09 19:50:59 +0900

    created basic design for message page, and single conversation

--- src/app/(home-like)/message/[messageId]/page.tsx
+++ src/app/(home-like)/message/[messageId]/page.tsx
@@ -1,6 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import React from "react";
-import { Separator } from "@/components/ui/separator";
-import ConversationList from "@/components/ConversationList";
 
 interface Params {
     messageId: string;
@@ -10,8 +10,17 @@ export default function MessagePage({
     params } : { params: Params }
 ) {
     return (
-        <div>
-            Message of {params.messageId}
+        <div className="flex flex-col relative w-[57vw] overflow-hidden">
+            <div className="flex w-full h-[15vh] bg-white">
+                <p className="text-black">Temp Header</p>
+            </div>
+            <div className="flex w-full h-[70vh] bg-blue-100">
+                Message of {params.messageId}
+            </div>
+            <div className="flex w-full h-[15vh] items-center bg-gray-400">
+                <Input placeholder="Type your message here" className="w-4/5 ml-10"/>
+                <Button className="ml-8">Send</Button>
+            </div>
         </div>        
     )
 }
