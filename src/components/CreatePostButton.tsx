7ab7b880cc61d7d40976297f9eb5414ab284b117
jameslee1237 <zcabjyl@ucl.ac.uk>
2024-06-02 16:02:22 +0900

    trying to add dropdown menu to allow deletion of post

--- src/components/CreatePostButton.tsx
+++ src/components/CreatePostButton.tsx
@@ -1,188 +1,201 @@
 "use client";
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import {
-    Dialog,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Separator } from "@/components/ui/separator"
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog_CP";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { useEffect, useState } from "react";
 import { Textarea } from "./ui/textarea";
 import { Button } from "@/components/ui/button";
 
 interface CreatePostButtonProps {
-    userId: string;
+  userId: string;
 }
 
-const bucketName = process.env.NEXT_PUBLIC_IAM_BUCKET_NAME
-const region = process.env.NEXT_PUBLIC_IAM_BUCKET_REGION
+const bucketName = process.env.NEXT_PUBLIC_IAM_BUCKET_NAME;
+const region = process.env.NEXT_PUBLIC_IAM_BUCKET_REGION;
 
 const FormSchema = z.object({
-    feed: z
-        .string()
-        .min(5, {
-            message: "Feed content must be at least 5 characters.",
-        })
-        .max(500, {
-            message: "Feed content must not be longer than 500 characters.",
-        }),
-})
-
-const CreatePostButton = (
-    props: CreatePostButtonProps
-) => {
-    const [img, setImg] = useState<File | null>(null);
-    const [encoded, setEncoded] = useState<string | null>("");
-    const [open, setOpen] = useState(false);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files) {
-            setImg(e.target.files[0]);
-        }
-        else {
-            setImg(null);
-        }
-    }
-
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
-        defaultValues: {
-            feed: "",
-        },
+  feed: z
+    .string()
+    .min(5, {
+      message: "Feed content must be at least 5 characters.",
     })
+    .max(500, {
+      message: "Feed content must not be longer than 500 characters.",
+    }),
+});
 
-    async function onSubmit(data: z.infer<typeof FormSchema>) {
-        try {
-            if (!img) {
-                return console.error("No image found");
-            }
-            const formData = new FormData();
-            formData.set("file", img);
+const CreatePostButton = (props: CreatePostButtonProps) => {
+  const [img, setImg] = useState<File | null>(null);
+  const [encoded, setEncoded] = useState<string | null>("");
+  const [open, setOpen] = useState(false);
 
-            const response = await fetch("/api/s3-upload", {
-                method: "POST",
-                body: formData,
-            });
-            if (!response.ok) {
-                throw new Error("Failed to upload image");
-            }
-
-            const result = await response.json();
-            const imageUrl = `https://${bucketName}.s3.${region}.amazonaws.com/${result.fileName}`
-            const postData = {
-                feed: data.feed,
-                url: imageUrl,
-                authorId: props.userId,
-            }
-
-            const postResponse = await fetch("/api/post/new", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(postData),
-            });
-            if (!postResponse.ok) {
-                throw new Error("Failed to create post");
-            }
-
-            setOpen(false);
-            form.reset();
-            setImg(null);
-
-        } catch (error) {
-            console.error(error);
-        }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setImg(e.target.files[0]);
+    } else {
+      setImg(null);
     }
+  };
 
-    const handleOpenChange = () => {
-        if (!open) {
-            setImg(null);
-            setEncoded(null);
-            form.reset();
-        }
-        setOpen(!open);
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      feed: "",
+    },
+  });
+
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    try {
+      if (!img) {
+        return console.error("No image found");
+      }
+      const formData = new FormData();
+      formData.set("file", img);
+
+      const response = await fetch("/api/s3-upload", {
+        method: "POST",
+        body: formData,
+      });
+      if (!response.ok) {
+        throw new Error("Failed to upload image");
+      }
+
+      const result = await response.json();
+      const imageUrl = `https://${bucketName}.s3.${region}.amazonaws.com/${result.fileName}`;
+      const postData = {
+        feed: data.feed,
+        url: imageUrl,
+        authorId: props.userId,
+      };
+
+      const postResponse = await fetch("/api/post/new", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      });
+      if (!postResponse.ok) {
+        throw new Error("Failed to create post");
+      }
+
+      setOpen(false);
+      form.reset();
+      setImg(null);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    useEffect(() => {
-        if (img) {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setEncoded(reader.result as string);
-            }
-            reader.readAsDataURL(img);
-        }
-        else {
-            setEncoded(null);
-        }
-    }), [img];
+  const handleOpenChange = () => {
+    if (!open) {
+      setImg(null);
+      setEncoded(null);
+      form.reset();
+    }
+    setOpen(!open);
+  };
 
-    return (
-        <div className="flex">
-            <Dialog open={open} onOpenChange={handleOpenChange}>
-                <DialogTrigger asChild>
-                    <div className="flex w-[15vw]">
-                        <button className="py-3 w-[100%] rounded-md mt-3 hover:bg-slate-500">
-                            <AddCircleIcon className="mr-2 mb-1"></AddCircleIcon>
-                            Create Post
-                        </button>
+  useEffect(() => {
+    if (img) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setEncoded(reader.result as string);
+      };
+      reader.readAsDataURL(img);
+    } else {
+      setEncoded(null);
+    }
+  }),
+    [img];
+
+  return (
+    <div className="flex">
+      <Dialog open={open} onOpenChange={handleOpenChange}>
+        <DialogTrigger asChild>
+          <div className="flex w-[15vw]">
+            <button className="py-3 w-[100%] rounded-md mt-3 hover:bg-slate-500">
+              <AddCircleIcon className="mr-2 mb-1"></AddCircleIcon>
+              Create Post
+            </button>
+          </div>
+        </DialogTrigger>
+        <DialogContent className="h-[80vh] w-[35vw]">
+          <div className="flex flex-col items-center h-full text-center">
+            <p className="text-xl font-bold mt-4">Create Post</p>
+            <Separator className="mt-4" />
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-full h-full space-y-6"
+              >
+                <FormField
+                  control={form.control}
+                  name="feed"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Textarea
+                          className="ml-0.5 ring-0 focus-visible:ring-0 rounded-none border-0 w-[99%] mb-6"
+                          placeholder="Write your own feed"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="flex flex-col justify-center">
+                  {encoded ? (
+                    <img
+                      src={encoded as string}
+                      alt="preview"
+                      className="object-contain max-h-[30vh]"
+                    />
+                  ) : (
+                    <div>
+                      <p>Select an image</p>
                     </div>
-                </DialogTrigger>
-                <DialogContent className="h-[80vh] w-[35vw]">
-                    <div className="flex flex-col items-center h-full text-center">
-                        <p className="text-xl font-bold mt-4">Create Post</p>
-                        <Separator className="mt-4" />
-                        <Form {...form}>
-                            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full h-full space-y-6">
-                                <FormField
-                                    control={form.control}
-                                    name="feed"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormControl>
-                                                <Textarea className="ml-0.5 ring-0 focus-visible:ring-0 rounded-none border-0 w-[99%] mb-6"
-                                                    placeholder="Write your own feed"
-                                                    {...field} />
-                                            </FormControl>
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                                <div className="flex flex-col justify-center">
-                                    {encoded ? <img src={encoded as string} alt="preview" className="object-contain max-h-[30vh]" />
-                                    : <div>
-                                        <p>Select an image</p>
-                                      </div>}
-                                </div>
-                                <div className="flex flex-col w-full justify-center items-center gap-1.5">
-                                    <Label htmlFor="picture">Picture</Label>
-                                    <Input id="picture" type="file" accept="image/*" onChange={handleChange} />
-                                    <Button type="submit" className="py-2 px-1 rounded-md bg-green-300">Save changes</Button>
-                                </div>
-                            </form>
-                        </Form>
-                    </div>
-                </DialogContent>
-            </Dialog>
-        </div>
-    )
-}
+                  )}
+                </div>
+                <div className="flex flex-col w-full justify-center items-center gap-1.5">
+                  <Label htmlFor="picture">Picture</Label>
+                  <Input
+                    id="picture"
+                    type="file"
+                    accept="image/*"
+                    onChange={handleChange}
+                  />
+                  <Button
+                    type="submit"
+                    className="py-2 px-1 rounded-md bg-green-300"
+                  >
+                    Save changes
+                  </Button>
+                </div>
+              </form>
+            </Form>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
 
 export default CreatePostButton;
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,193 +1,213 @@
 import Image from "next/image";
-import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage,
-} from "@/components/ui/avatar";
-import {
-    Dialog,
-    DialogContent,
-    DialogTrigger,
-} from "@/components/ui/dialog"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import React, { useEffect, useState, ChangeEvent } from "react";
 import { ScrollArea } from "./ui/scroll-area";
 import { Separator } from "./ui/separator";
 import Comment from "./Comment";
-import Input from '@mui/material/Input';
+import Input from "@mui/material/Input";
 import { EllipsisVerticalIcon } from "lucide-react";
 import { getCurrentUser } from "@/app/actions/getCurrentUser";
 import { Skeleton } from "./ui/skeleton";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 interface PostCardProps {
-    image: string;
-    postid: string;
+  image: string;
+  postid: string;
 }
 
 const PostCard = ({ image, postid }: PostCardProps) => {
-    const id = getCurrentUser().userData.id;
-    const userName = getCurrentUser().userData.userName;
-    const profile = getCurrentUser().userData.profileImage;
-    const [comments, setComments] = useState<string[] | null>(null);
-    const [authorIds, setAuthorIds] = useState<string[] | null>(null);
-    const [fullval, setfullVal] = useState("");
-    const [fulltyped, setfullTyped] = useState(false);
-    const [dialogOpen, setDialogOpen] = useState(false);
-    const [portrait, setPortrait] = useState<boolean | null>(null);
+  const id = getCurrentUser().userData.id;
+  const userName = getCurrentUser().userData.userName;
+  const profile = getCurrentUser().userData.profileImage;
+  const [comments, setComments] = useState<string[] | null>(null);
+  const [authorIds, setAuthorIds] = useState<string[] | null>(null);
+  const [fullval, setfullVal] = useState("");
+  const [fulltyped, setfullTyped] = useState(false);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [portrait, setPortrait] = useState<boolean | null>(null);
 
-    const hasTypedFull = (e: ChangeEvent<HTMLInputElement>) => {
-        const typedValue = e.target.value;
-        setfullTyped(typedValue.length > 0);
-        setfullVal(typedValue);
+  const hasTypedFull = (e: ChangeEvent<HTMLInputElement>) => {
+    const typedValue = e.target.value;
+    setfullTyped(typedValue.length > 0);
+    setfullVal(typedValue);
+  };
+
+  const handleDialogOpenChange = () => {
+    if (dialogOpen) {
+      setfullVal("");
+      setfullTyped(false);
     }
+    setDialogOpen((prev) => !prev);
+  };
+  const handlefullValComment = async () => {
+    try {
+      const commentData = {
+        comment: fullval,
+        postId: postid,
+        authorId: id,
+      };
+      const res = await fetch("/api/comment/new", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(commentData),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to create comment");
+      }
+      setfullVal("");
 
-    const handleDialogOpenChange = () => {
-        if (dialogOpen) {
-            setfullVal("");
-            setfullTyped(false);
-        }
-        setDialogOpen((prev) => !prev);
+      await getComments(postid);
+    } catch (error) {
+      console.log(error);
     }
-    const handlefullValComment = async () => {
-        try {
-            const commentData = {
-                comment: fullval,
-                postId: postid,
-                authorId: id,
-            }
-            const res = await fetch("/api/comment/new", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(commentData),
-            });
-            if (!res.ok) {
-                throw new Error("Failed to create comment");
-            }
-            setfullVal("");
+  };
 
-            await getComments(postid);
-
-        } catch (error) {
-            console.log(error);
-        }
+  const getComments = async (p_id: string) => {
+    try {
+      const res = await fetch(`/api/comment?postId=${p_id}`);
+      if (!res.ok) {
+        throw new Error("Failed to fetch comments");
+      }
+      const data = await res.json();
+      setComments(data.comments.map((comment: any) => comment.comment));
+      setAuthorIds(data.comments.map((comment: any) => comment.authorId));
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const getComments = async (p_id: string) => {
-        try {
-            const res = await fetch(`/api/comment?postId=${p_id}`);
-            if (!res.ok) {
-                throw new Error("Failed to fetch comments");
-            }
-            const data = await res.json();
-            setComments(data.comments.map((comment: any) => comment.comment));
-            setAuthorIds(data.comments.map((comment: any) => comment.authorId));
-        } catch (error) {
-            console.log(error);
-        }
+  useEffect(() => {
+    getComments(postid);
+  }, [postid]);
+
+  useEffect(() => {
+    if (image === "") {
+      return;
     }
+    const img = new window.Image();
+    img.src = image;
+    img.onload = () => {
+      setPortrait(img.width < img.height);
+    };
+  }, [image]);
 
-    useEffect(() => {
-        getComments(postid);
-    }, [postid]);
-
-    useEffect(() => {
-        if (image === "") {
-            return;
-        }
-        const img = new window.Image();
-        img.src = image;
-        img.onload = () => {
-            setPortrait(img.width < img.height);
-        };
-    }, [image])
-
-    if (comments === null || authorIds === null) {
-        return (
-            <div>
-                <Skeleton className="w-[300px] h-[200px] rounded-md" />
-            </div>
-        )
-    }
-
+  if (comments === null || authorIds === null) {
     return (
-        <div className="flex w-[300px] h-[200px] bg-white">
-            <button onClick={() => setDialogOpen(true)} className="w-full">
-                <div className="w-full h-full overflow-hidden">
-                    <Image
-                        src={image}
-                        alt="image"
-                        sizes="20vw"
-                        width={600}
-                        height={400}
-                        priority={true}
-                    />
-                </div>
-            </button>
-            <div className="flex h-full">
-                <Dialog open={dialogOpen} onOpenChange={handleDialogOpenChange}>
-                    <DialogTrigger asChild>
-                    </DialogTrigger>
-                    <DialogContent>
-                        <div className="flex h-full">
-                            <div className="flex h-full bg-black items-center justify-center w-1/2">
-                                <button>
-                                    <EllipsisVerticalIcon className="absolute top-4 right-2" />
-                                </button>
-                                <Image
-                                    src={image}
-                                    alt="test"
-                                    sizes="512px"
-                                    style={{ 
-                                        width: portrait ? "auto" : "100%",
-                                        height: portrait ? "100%" : "auto"
-                                    }}
-                                    priority={true}
-                                    width={2500}
-                                    height={1668}
-                                />
-                            </div>
-                            <div className="flex flex-col w-1/2 max-h-full">
-                                <div className="flex ml-4 mt-4 mb-4">
-                                    <Avatar className="hidden h-9 w-9 sm:flex">
-                                        <AvatarImage src={profile} alt="Avatar" />
-                                        <AvatarFallback className="bg-green-200">OM</AvatarFallback>
-                                    </Avatar>
-                                    <div className="grid gap-1">
-                                        <h1 className="text-[16px] ml-2 font-bold text-black text-muted-foreground mt-1">
-                                            {userName}
-                                        </h1>
-                                    </div>
-                                </div>
-                                <Separator />
-                                <div className="flex w-full h-[75%] max-h-full">
-                                    <ScrollArea className="w-full mt-4 mb-4 max-h-[75vh] overflow-hidden">
-                                        {comments.map((commentf, index) => (
-                                            <div key={index}>
-                                                <Comment
-                                                    comment={commentf}
-                                                    authorId={authorIds[index]} />
-                                                <Separator className="mt-4" />
-                                            </div>
-                                        ))}
-                                    </ScrollArea>
-                                </div>
-                                <Separator />
-                                <div className="flex mt-4 mx-4">
-                                    <Input fullWidth={true} value={fullval} placeholder="Add a comment..." onChange={hasTypedFull} />
-                                    {fulltyped && (
-                                        <button onClick={handlefullValComment} className="bg-green-400 hover:bg-green-700 rounded-md px-2 py-1 ml-4">
-                                            Post
-                                        </button>
-                                    )}
-                                </div>
-                            </div>
-                        </div>
-                    </DialogContent>
-                </Dialog>
-            </div>
-        </div>
+      <div>
+        <Skeleton className="w-[300px] h-[200px] rounded-md" />
+      </div>
     );
-}
+  }
+
+  return (
+    <div className="flex w-[300px] h-[200px] bg-white">
+      <button onClick={() => setDialogOpen(true)} className="w-full">
+        <div className="w-full h-full overflow-hidden">
+          <Image
+            src={image}
+            alt="image"
+            sizes="20vw"
+            width={600}
+            height={400}
+            priority={true}
+          />
+        </div>
+      </button>
+      <div className="flex h-full">
+        <Dialog open={dialogOpen} onOpenChange={handleDialogOpenChange}>
+          <DialogTrigger asChild></DialogTrigger>
+          <DialogContent>
+            <div className="flex h-full">
+              <div className="flex h-full bg-black items-center justify-center w-1/2">
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <button>
+                      <EllipsisVerticalIcon className="absolute top-4 right-2" />
+                    </button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="absolute -top-[300px] -right-[1000px]">
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Billing</DropdownMenuItem>
+                    <DropdownMenuItem>Team</DropdownMenuItem>
+                    <DropdownMenuItem>Subscription</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+
+                <Image
+                  src={image}
+                  alt="test"
+                  sizes="512px"
+                  style={{
+                    width: portrait ? "auto" : "100%",
+                    height: portrait ? "100%" : "auto",
+                  }}
+                  priority={true}
+                  width={2500}
+                  height={1668}
+                />
+              </div>
+              <div className="flex flex-col w-1/2 max-h-full">
+                <div className="flex ml-4 mt-4 mb-4">
+                  <Avatar className="hidden h-9 w-9 sm:flex">
+                    <AvatarImage src={profile} alt="Avatar" />
+                    <AvatarFallback className="bg-green-200">OM</AvatarFallback>
+                  </Avatar>
+                  <div className="grid gap-1">
+                    <h1 className="text-[16px] ml-2 font-bold text-black text-muted-foreground mt-1">
+                      {userName}
+                    </h1>
+                  </div>
+                </div>
+                <Separator />
+                <div className="flex w-full h-[75%] max-h-full">
+                  <ScrollArea className="w-full mt-4 mb-4 max-h-[75vh] overflow-hidden">
+                    {comments.map((commentf, index) => (
+                      <div key={index}>
+                        <Comment
+                          comment={commentf}
+                          authorId={authorIds[index]}
+                        />
+                        <Separator className="mt-4" />
+                      </div>
+                    ))}
+                  </ScrollArea>
+                </div>
+                <Separator />
+                <div className="flex mt-4 mx-4">
+                  <Input
+                    fullWidth={true}
+                    value={fullval}
+                    placeholder="Add a comment..."
+                    onChange={hasTypedFull}
+                  />
+                  {fulltyped && (
+                    <button
+                      onClick={handlefullValComment}
+                      className="bg-green-400 hover:bg-green-700 rounded-md px-2 py-1 ml-4"
+                    >
+                      Post
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
+  );
+};
 
 export default PostCard;
