3cb97ef7992c165e59973bc1825bc9ae38098617
jameslee1237 <zcabjyl@ucl.ac.uk>
2024-06-15 01:35:18 +0900

    comment api was not fixed

--- src/app/api/comment/route.ts
+++ src/app/api/comment/route.ts
@@ -1,22 +1,26 @@
 import prisma from "@/lib/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest){
-    try {
-        const { searchParams } = new URL(req.url);
-        const postId = searchParams.get("postId");
-        if (!postId) {
-            return NextResponse.json({ error: "postId is required" }, { status: 400 });
-        }
-
-        const comments = await prisma.comment.findMany({
-            where: {
-                postId: postId
-            },
-        });
-        return NextResponse.json({ comments });
-
-    } catch (error) {
-        return NextResponse.json({ error: error instanceof Error ? error.message : String(error) });
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const postId = searchParams.get("postId");
+  try {
+    if (!postId) {
+      return NextResponse.json(
+        { error: "postId is required" },
+        { status: 400 }
+      );
     }
+
+    const comments = await prisma.comment.findMany({
+      where: {
+        postId: postId,
+      },
+    });
+    return NextResponse.json({ comments });
+  } catch (error) {
+    return NextResponse.json({
+      error: error instanceof Error ? error.message : String(error),
+    });
+  }
 }
