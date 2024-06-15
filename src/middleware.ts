1757d80105394197163bc0d542260450159a7abb
jameslee1237 <zcabjyl@ucl.ac.uk>
2024-06-15 21:44:37 +0900

    updating to check prisma

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,30 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 import { NextResponse, NextRequest } from "next/server";
- 
+
 export default authMiddleware({
   publicRoutes: ["/"],
-  ignoredRoutes: ['/api/webhooks(.*)'],
+  ignoredRoutes: ["/api/webhooks(.*)"],
   afterAuth(auth, req, evt) {
-    if (auth.userId && (req.nextUrl.pathname === "/sign-in" || req.nextUrl.pathname === "/sign-up")) {
-      return NextResponse.redirect(new URL('/main', req.url));
+    if (
+      auth.userId &&
+      (req.nextUrl.pathname === "/sign-in" ||
+        req.nextUrl.pathname === "/sign-up")
+    ) {
+      return NextResponse.redirect(new URL("/main", req.url));
     }
 
-    // if (!auth.userId && (req.url.includes("/main") || req.url.includes("/message") || req.url.includes("/user-page"))){
-    //   return NextResponse.redirect(new URL('/', req.url))
-    // }
+    if (
+      !auth.userId && !auth.isPublicRoute
+    ) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
 
-    // if (auth.userId && req.nextUrl.pathname === "/") {
-    //   return NextResponse.redirect(new URL('/main', req.url));
-    // }
-  }
+    if (auth.userId && req.nextUrl.pathname === "/") {
+      return NextResponse.redirect(new URL("/main", req.url));
+    }
+  },
 });
- 
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
