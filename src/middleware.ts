f3fed14d1d824907940852eeb1cf943e13705ab5
jameslee1237 <zcabjyl@ucl.ac.uk>
2024-06-15 21:08:22 +0900

    checking

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,13 +5,9 @@ export default authMiddleware({
   publicRoutes: ['/', '/sign-in', '/sign-up'],
   ignoredRoutes: ['/api/webhooks(.*)'],
   afterAuth(auth, req, evt) {
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url});
+    if (auth.userId && (req.nextUrl.pathname === "/sign-in" || req.nextUrl.pathname === "/sign-up")) {
+      return NextResponse.redirect(new URL('/main', req.url));
     }
-
-    // if (auth.userId && (req.nextUrl.pathname === "/sign-in" || req.nextUrl.pathname === "/sign-up")) {
-    //   return NextResponse.redirect(new URL('/main', req.url));
-    // }
 
     // if (!auth.userId && (req.url.includes("/main") || req.url.includes("/message") || req.url.includes("/user-page"))){
     //   return NextResponse.redirect(new URL('/', req.url))
